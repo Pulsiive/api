e5f8dafa83208df3cf22b24ecbf5fcd7410b1282
Christophe Sourinthone <58895942+Shylk@users.noreply.github.com>
2023-01-13 02:21:35 +0100

    Update api.ts

--- src/Routes/api.ts
+++ src/Routes/api.ts
@@ -45,7 +45,7 @@ router.post('/api/v1/profile/station', AuthMiddleware, PrivateStationController.
 router.put('/api/v1/profile/station/:id', AuthMiddleware, PrivateStationController.update);
 router.delete('/api/v1/profile/station/:id', AuthMiddleware, PrivateStationController.delete);
 
-router.get('/api/v1/stations', AuthMiddleware, StationController.getFromParams);
+router.post('/api/v1/stations', AuthMiddleware, StationController.getFromParams);
 router.get('/api/v1/stations/all', StationController.getAll);
 
 router.post('/api/v1/emailVerification', EmailVerificationController.request);
