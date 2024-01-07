5e0638db3c319e2491e45b140718e95d20190965
ILamrani <64532836+IliasLamrani@users.noreply.github.com>
2024-01-07 14:00:01 +0100

    Update NotificationService.ts

--- src/Services/Notification/NotificationService.ts
+++ src/Services/Notification/NotificationService.ts
@@ -3,12 +3,12 @@ import prisma from '../../../prisma/client';
 import { ApiError } from '../../Errors/ApiError';
 
 admin.initializeApp({
-  credential: admin.credential.cert({
+  credential: {
     type: process.env.FIREBASE_TYPE,
     projectId: process.env.FIREBASE_PROJECT_ID,
     privateKey: process.env.FIREBASE_PRIVATE_KEY,
     clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-  } as any)
+  } as any
 });
 
 class NotificationService {
