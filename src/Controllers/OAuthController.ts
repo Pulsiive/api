975a6b915b3503ff6b55a542b00d30d48f37c8af
Ilias Lamrani <ilias.lamrani@epitech.eu>
2024-01-10 22:06:31 +0100

    feat: add fcmkToken to user google auth register

--- src/Controllers/OAuthController.ts
+++ src/Controllers/OAuthController.ts
@@ -1,22 +1,22 @@
 import express from 'express';
 import OAuthService from '../Services/OAuth/OAuthService';
 import { errorWrapper } from '../Utils/errorWrapper';
-import {ApiError} from "../Errors/ApiError";
+import { ApiError } from '../Errors/ApiError';
 
 class OAuthController {
   static async register(req: express.Request, res: express.Response) {
     try {
-      if (!req.body.credential)
-        throw new ApiError('Error: Google ID token not provided', 422);
+      if (!req.body.credential) throw new ApiError('Error: Google ID token not provided', 422);
 
-        const verificationResponse = await OAuthService.verifyGoogleToken(req.body.credential);
+      const verificationResponse = await OAuthService.verifyGoogleToken(req.body.credential);
 
-        const profile = verificationResponse?.payload;
+      const profile = verificationResponse?.payload;
 
       const userData = await OAuthService.register(req.body.credential, {
         email: profile?.email,
         firstName: profile?.given_name,
         lastName: profile?.family_name,
+        fcmToken: req.body.fcmToken
       });
 
       return res.json(userData);
@@ -27,8 +27,7 @@ class OAuthController {
 
   static async login(req: express.Request, res: express.Response) {
     try {
-      if (!req.body.credential)
-        throw new ApiError('Error: Google ID token not provided', 422);
+      if (!req.body.credential) throw new ApiError('Error: Google ID token not provided', 422);
 
       const verificationResponse = await OAuthService.verifyGoogleToken(req.body.credential);
 
